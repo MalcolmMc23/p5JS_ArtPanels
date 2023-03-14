7fb9c6fda737ba96f17626a4ad43dc00090e3a94
Malcolm McDonald <malcolmemcdonald@gmail.com>
2023-03-14 14:24:58 -0700

    adding new buttons
    I removed the html buttons and made my own Button class. this will make it easier to customise them.

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 // Global variables will be created in solidity. Here not for ease of testing.
 // Artwork panel
 let artWorkIPFS = "Qmb4obBzUofeZtcUxhEWUYCmhneZMzCrBQrNGnJnqKNyDq"
-let scoreCardIPFS= "QmTSBSHXJgnVg16pjxnH3CAqkL4QDtBwr6aSHkbvhZFmR3"
+let scoreCardIPFS = "QmTSBSHXJgnVg16pjxnH3CAqkL4QDtBwr6aSHkbvhZFmR3"
 let gameIPFS = "QmTSBSHXJgnVg16pjxnH3CAqkL4QDtBwr6aSHkbvhZFmR3"
 
 // Game panel text
@@ -20,8 +20,7 @@ let burnToClaim = '69.4201'; // Ξ
 // End globals 
 
 // use WASD to move freely around the page
-let page, camLoc, buttLeft, buttRight, timer
-let buttL, buttR;
+let page, camLoc, buttL, buttR, timer
 let pages = [];
 let numOfPages = 3;
 let movingRight = false;
@@ -31,7 +30,8 @@ let artWorkPanel = "https://gateway.pinata.cloud/ipfs/" + artWorkIPFS;
 let scoreCardPanel = "https://gateway.pinata.cloud/ipfs/" + scoreCardIPFS;
 let gamePanel = "https://gateway.pinata.cloud/ipfs/" + gameIPFS;
 
-let defifaBlue = [19,228,240];
+let defifaBlue = [19, 228, 240];
+
 
 let txt1 = [
   ["Prize Pool: ", defifaBlue],
@@ -57,12 +57,12 @@ let txt5 = [
   ["Burn to Claim: ", defifaBlue],
   [burnToClaim, defifaBlue],
   ["", defifaBlue]
-]; 
+];
 let txt6 = [
   ["Team: ", defifaBlue],
   [tierName, defifaBlue],
   ["", defifaBlue]
-]; 
+];
 let pageImg = []
 
 function preload() {
@@ -75,37 +75,29 @@ function setup() {
   createCanvas(400, 400);
   camLoc = createVector(0, 0); // camLoc just means camera location. this will be the location of the canvas.
   for (let i = 0; i < numOfPages; i++) {
-    buttLeft = createButton("Artwork"); 
-    buttLeft.position(10, canvas.height/2 - 30)
-    buttRight = createButton("Score Card"); // creates a button
-    buttRight.style('background-color', color(defifaBlue));
-    buttRight.position(canvas.width/2 - 90, canvas.height/2 - 30) // sets the button in the bottem right
-    for (let i = 0; i < numOfPages; i++) {
-      pages[i] = new Page(canvas.width / 2 * i, 0, i, pageImg[i]);
-    }
+    pages[i] = new Page(canvas.width / 2 * i, 0, i, pageImg[i]);
   }
   timer = canvas.width / 2
 
-  buttL = new Button(100, 100, 100, 50, 100);
-  // buttR = new Button(x, y, w, h, c);
+  buttL = new Button(10, canvas.height / 2 - 30, 50, 25, "<");
+  buttR = new Button(canvas.width / 2 - 90, canvas.height / 2 - 30, 50, 25, ">");
 
 }
 
 function draw() {
   background(220);
-  buttRight.mousePressed(goRight) // right button listener
-  buttLeft.mousePressed(goLeft) // left button listener
-
   slide()
+
   push();
   translate(camLoc.x, camLoc.y); // translates the view of your page (AKA the canvas) to the camLoc
-
 
   for (let i = 0; i < numOfPages; i++) {
     pages[i].run();
   }
-  buttL.run()
+
   pop();
+  buttL.run()
+  buttR.run()
 }
 function goRight() {
   if (!movingLeft && !isPaused) {
@@ -140,9 +132,9 @@ function slide() {
   //checks if the camLoc is past the fist page and sets it to 0 if true.
   if (-camLoc.x <= 0) {
     camLoc.x = 0;
-    buttLeft.position(-100, canvas.height / 2 - 30)// sets the button outside the view of the page
+    buttL.loc.x = -100 // sets the location of the left button out of view
   } else {
-    buttLeft.position(10, canvas.height / 2 - 30)// sets the button in the bottem left
+    buttL.loc.x = 10, canvas.height / 2 - 30 // sets the location of the left button inside the view of the page
   }
   // checks if the camLoc is past the last page and set it to the loc of hte last page if it is.
   if (-camLoc.x >= pages[pages.length - 1].loc.x) {
@@ -150,9 +142,9 @@ function slide() {
     movingRight = false;
     movingLeft = false;
     camLoc.x = -pages[pages.length - 1].loc.x
-    buttRight.position(-100, canvas.height / 2 - 30) // sets the button in the bottem right
+    buttR.loc.x = -100
   } else {
-    buttRight.position(canvas.width / 2 - 90, canvas.height / 2 - 30) // sets the button out side of the view of the page
+    buttR.loc.x = canvas.height / 2 - 90 // sets the location of the right button inside the view of the page
 
   }
 
@@ -172,51 +164,71 @@ function keyPressed() { //key listener for moving with w a s d
   }
 }
 
-function drawtext( x, y, text_array ) {
-  
+function drawtext(x, y, text_array) {
+
   var pos_x = x;
-  for ( var i = 0; i < text_array.length; ++ i ) {
-      var part = text_array[i];
-      var t = part[0];
-      var c = part[1];
-      var w = textWidth( t );
-      fill( c );
-      text( t, pos_x, y);
-      pos_x += w;
+  for (var i = 0; i < text_array.length; ++i) {
+    var part = text_array[i];
+    var t = part[0];
+    var c = part[1];
+    var w = textWidth(t);
+    fill(c);
+    text(t, pos_x, y);
+    pos_x += w;
   }
 }
+
+function mousePressed() { // mouse pressed listener
+  if ( // checks if the mouse is over the left button
+    mouseX > buttL.loc.x &&
+    mouseX < buttL.loc.x + buttL.w &&
+    mouseY > buttL.loc.y &&
+    mouseY < buttL.loc.y + buttL.h
+  ) {
+    goLeft();
+  }
+  if ( // checks if the mouse is over the right button
+    mouseX > buttR.loc.x &&
+    mouseX < buttR.loc.x + buttR.w &&
+    mouseY > buttR.loc.y &&
+    mouseY < buttR.loc.y + buttR.h
+  ) {
+    goRight();
+  }
+}
+
 
 class Page {
   constructor(x, y, pageIndex, img) {
     this.loc = createVector(x, y); // the x and y are the top left of the page
     // for some reason the canvas width and height is double what is suposted to be so i just divided it by 2
-    this.w = canvas.width/2; 
-    this.h = canvas.height/2;
+    this.w = canvas.width / 2;
+    this.h = canvas.height / 2;
     this.pageNum = pageIndex + 1; // added one so you start at page 1 not page 0
     this.img = img;
   }
 
   run() {
     // image(this.img, this.loc.x, this.loc.y, canvas.width/2, canvas.heigh/2)
-    image(this.img, this.loc.x, this.loc.y,400, 400)
+    image(this.img, this.loc.x, this.loc.y, 400, 400)
 
     // just shows the text
     textSize(20);
     textAlign(LEFT);
     textFont(myFont);
-    
+
     //drawtext(this.loc.x + 112, this.loc.y + 30, string );
     //KMac clean this up, dry
-    if(this.pageNum==2){
-      drawtext(this.loc.x + 112, this.loc.y + 30, txt6 );
-      drawtext(this.loc.x + 112, this.loc.y + 70, txt1 );
-      drawtext(this.loc.x + 112, this.loc.y + 110, txt2 );
-      drawtext(this.loc.x + 112, this.loc.y + 150, txt3 );
-      drawtext(this.loc.x + 112, this.loc.y + 190, txt4 );
-      drawtext(this.loc.x + 112, this.loc.y + 230, txt5 );
+    if (this.pageNum == 2) {
+      drawtext(this.loc.x + 112, this.loc.y + 30, txt6);
+      drawtext(this.loc.x + 112, this.loc.y + 70, txt1);
+      drawtext(this.loc.x + 112, this.loc.y + 110, txt2);
+      drawtext(this.loc.x + 112, this.loc.y + 150, txt3);
+      drawtext(this.loc.x + 112, this.loc.y + 190, txt4);
+      drawtext(this.loc.x + 112, this.loc.y + 230, txt5);
     };
-    if(this.pageNum==3){
-    // to do set text and buttons
+    if (this.pageNum == 3) {
+      // to do set text and buttons
     }
 
     // makes a box around the page
@@ -227,11 +239,12 @@ class Page {
   }
 }
 class Button {
-  constructor(x, y, w, h, c) {
+  constructor(x, y, w, h, t) {
     this.loc = new createVector(x, y);
     this.w = w;
     this.h = h;
-    this.clr = "green"
+    this.text = t;
+    this.clr = "white"
   }
   run() {
     this.render()
@@ -239,8 +252,13 @@ class Button {
   }
   render() {
     fill(this.clr)
+    stroke(20)
+    strokeWeight(2)
+    rect(this.loc.x, this.loc.y, this.w, this.h, 10)
+    fill("black")
     noStroke()
-    rect(this.loc.x, this.loc.y, this.w, this.h, 20)
+    textSize(15)
+    text(this.text, this.loc.x + (this.w / 2) - 5, this.loc.y + (this.h / 2) + 3)
   }
   checkMouse() {
     if (
@@ -249,10 +267,9 @@ class Button {
       mouseY > this.loc.y &&
       mouseY < this.loc.y + this.h
     ) {
-      this.clr = "red"
+      this.clr = "gray"
     } else {
-      this.clr = "green"
-
+      this.clr = "white"
     }
   }
 }
